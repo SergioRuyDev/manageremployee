9340b4509d4334704c864678246fdd84fe477f21
SergioRuyDev <sergioruyenator@gmail.com>
2021-02-13 08:47:30 +0400

    Initial commit

---
+++ src/main/java/com/sergioruy/manageremployee/ManageremployeeApplication.java
@@ -0,0 +1,13 @@
+package com.sergioruy.manageremployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageremployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageremployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sergioruy/manageremployee/ManageremployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioruy.manageremployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageremployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

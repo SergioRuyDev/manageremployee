48f1c040ee2129365fea902859403906c0c05dd6
SergioRuyDev <sergioruyenator@gmail.com>
2021-02-13 09:16:07 +0400

    Starting Service class.

--- src/main/java/com/sergioruy/manageremployee/service/EmployeeService.java
+++ src/main/java/com/sergioruy/manageremployee/service/EmployeeService.java
@@ -1,13 +1,23 @@
 package com.sergioruy.manageremployee.service;
 
+import com.sergioruy.manageremployee.model.Employee;
 import com.sergioruy.manageremployee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
+    @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
     }
 }

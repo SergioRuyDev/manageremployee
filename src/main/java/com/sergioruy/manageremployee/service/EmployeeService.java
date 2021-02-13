630ce8f12e7f2accdbf4296077b46fb529851a82
SergioRuyDev <sergioruyenator@gmail.com>
2021-02-13 09:13:34 +0400

    Repository Employee is created.

---
+++ src/main/java/com/sergioruy/manageremployee/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.sergioruy.manageremployee.service;
+
+import com.sergioruy.manageremployee.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+}

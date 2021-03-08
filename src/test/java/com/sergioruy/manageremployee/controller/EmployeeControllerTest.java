73f70335105620aa3e77d6d837c254b571205c42
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-08 17:50:30 +0400

    Change the repository.

---
+++ src/test/java/com/sergioruy/manageremployee/controller/EmployeeControllerTest.java
@@ -0,0 +1,26 @@
+package com.sergioruy.manageremployee.controller;
+
+import com.sergioruy.manageremployee.model.Employee;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerTest {
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void employeeAll() {
+
+    }
+
+
+}

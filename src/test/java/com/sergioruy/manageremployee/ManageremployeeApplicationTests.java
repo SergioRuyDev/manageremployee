74e33eb2cbd8fcea75f9aff9d4d652ebf951dbb6
SergioRuyDev <sergioruyenator@gmail.com>
2021-11-17 17:24:02 +0400

    Practice some integration tests.

--- src/test/java/com/sergioruy/manageremployee/ManageremployeeApplicationTests.java
+++ src/test/java/com/sergioruy/manageremployee/ManageremployeeApplicationTests.java
@@ -1,46 +1,28 @@
 package com.sergioruy.manageremployee;
 
-import com.sergioruy.manageremployee.model.Employee;
+import com.sergioruy.manageremployee.exception.UserNotFoundException;
 import com.sergioruy.manageremployee.service.EmployeeService;
-import lombok.AllArgsConstructor;
-import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AllArgsConstructor
 class ManageremployeeApplicationTests {
 
-    private final EmployeeService employeeService;
-
-//    @Test
-//    public void testRegisterEmployeeName() {
-//        Employee newEmployee = new Employee();
-//        newEmployee.setName("Sergio");
-//
-//        newEmployee = employeeService.addEmployee(newEmployee);
-//
-//        assertThat(newEmployee).isNotNull();
-//        assertThat(newEmployee.getId()).isNotNull();
-//    }
+    @Autowired
+    private EmployeeService employeeService;
 
     @Test
-    public void testEmployeeNoName() {
-        Employee newEmployee = new Employee();
-        newEmployee.setName(null);
+    public void when_Employee_NotExist() {
 
-        ConstraintViolationException errorExpected =
-                Assertions.assertThrows(ConstraintViolationException.class, () -> {
-                    employeeService.addEmployee(newEmployee);
-                });
-
-        assertThat(errorExpected).isNotNull();
+        UserNotFoundException errorExpected =
+            Assertions.assertThrows(UserNotFoundException.class, () -> {
+                employeeService.deleteEmployee(100L);
+            });
     }
 }
